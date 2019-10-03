e09d2ba7d5bc441adf2809597793d2f7774141e2
unknown <boukernineabdellatif@gmail.com>
2019-10-03 20:22:27 +0200

    fin

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+      $('.ui.dropdown')
+      .dropdown({
+        on: 'hover'
+        })
+         ;
+
+      $('.tabular.menu .item').tab();
+
+
+      $('.overlay').visibility({
+
+        type: 'fixed'
+
+      });
+
+        // popup
+
+      $('.fa-github')
+        .popup({
+          inline: true
+        })
+      ;
+      $('.fa-google')
+        .popup({
+          inline: true
+        })
+      ;
+      $('.fa-twitter')
+        .popup({
+          inline: true
+        })
+      ;
+      $('.fa-instagram')
+        .popup({
+          inline: true
+        })
+      ;
+      $('.univc2')
+        .popup({
+          inline: true
+        })
+      ;
+       //fin popup
